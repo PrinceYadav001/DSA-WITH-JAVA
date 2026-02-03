efba7c8c46c1b6f0a65771ddd7b3e6fc035696f7
Prince Yadav <150104887+PrinceYadav001@users.noreply.github.com>
2026-02-03 19:23:30 +0530

    Time: 6 ms (14.32%) | Memory: 49.4 MB (5.62%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,33 +1,33 @@
-// class Solution {
-//     public int[] twoSum(int[] numbers, int target) {
-//         HashMap<Integer, Integer> map = new HashMap<>();
-//         for(int i =0 ; i<numbers.length ; i++){
-//             int n = target-numbers[i];
-//             if(map.containsKey(n)){
-//                 return new int[] {map.get(n) , i+1};
-//             }
-//             map.put(numbers[i] , i+1);
-//         }
-//         return new int[] { };
-//     }
-// }
-
 class Solution {
-    public int[] twoSum(int[] numbers , int target){
-        int i =0 ; 
-        int j = numbers.length-1;
-        
-        while(i<j){
-            int sum = numbers[i]+numbers[j];
-            if(sum == target){
-                return new int[]{i+1 , j+1};
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i =0 ; i<numbers.length ; i++){
+            int n = target-numbers[i];
+            if(map.containsKey(n)){
+                return new int[] {map.get(n) , i+1};
             }
-            if(sum<target){
-                i++;
-            }else if(sum>target){
-                j--;
-            }    
+            map.put(numbers[i] , i+1);
         }
         return new int[] { };
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] numbers , int target){
+//         int i =0 ; 
+//         int j = numbers.length-1;
+        
+//         while(i<j){
+//             int sum = numbers[i]+numbers[j];
+//             if(sum == target){
+//                 return new int[]{i+1 , j+1};
+//             }
+//             if(sum<target){
+//                 i++;
+//             }else if(sum>target){
+//                 j--;
+//             }    
+//         }
+//         return new int[] { };
+//     }
+// }
