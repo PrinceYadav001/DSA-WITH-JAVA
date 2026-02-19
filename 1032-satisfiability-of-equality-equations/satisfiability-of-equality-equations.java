5b126f5101aa64c285aaad121eac391a11fc3a06
Prince Yadav <150104887+PrinceYadav001@users.noreply.github.com>
2026-02-19 20:54:20 +0530

    Time: 1 ms (96.45%) | Memory: 43.8 MB (75.03%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,62 +1,44 @@
 class Solution {
     public boolean equationsPossible(String[] equations) {
-
         char parent[] = new char[26];
         int rank[] = new int[26];
-
-        for(int i = 0; i < 26; i++){
-            parent[i] = (char)('a' + i);
-            rank[i] = 0;
+        for(int i =0 ; i<26 ; i++){
+            parent[i]=(char)('a'+i);
+            rank[i]=0;
         }
-
-        // Step 1: Process all ==
         for(String str : equations){
-            if(str.charAt(1) == '='){
-                union(str.charAt(0), str.charAt(3), parent, rank);
+            if(str.charAt(1)=='='){
+                union(str.charAt(0) , str.charAt(3), parent , rank);
             }
         }
-
-        // Step 2: Process all !=
         for(String str : equations){
-            if(str.charAt(1) == '!'){
+            if(str.charAt(1)=='!'){
                 char pa = find(str.charAt(0), parent);
                 char pb = find(str.charAt(3), parent);
-                if(pa == pb){
-                    return false;   // contradiction found
+                if(pa==pb){
+                    return false;
                 }
             }
         }
-
         return true;
     }
-
-    public static char find(char a, char parent[]){
-        int idx = a - 'a';
-        if(parent[idx] == a){
+    public static char find(char a , char parent[]){
+        if(a==parent[a-'a']){
             return a;
         }
-        parent[idx] = find(parent[idx], parent);
-        return parent[idx];
+        return parent[a-'a']= find(parent[a-'a'] , parent);
     }
-
-    public static void union(char a, char b, char parent[], int rank[]){
-        char pa = find(a, parent);
-        char pb = find(b, parent);
-
-        if(pa == pb) return;
-
-        int ia = pa - 'a';
-        int ib = pb - 'a';
-
-        if(rank[ia] > rank[ib]){
-            parent[ib] = pa;
-        }
-        else if(rank[ia] < rank[ib]){
-            parent[ia] = pb;
-        }
-        else{
-            parent[ia] = pb;
-            rank[ib]++;
+    public static void union(char a , char b , char parent[] , int rank[]){
+        char pa  = find(a , parent);
+        char pb = find(b , parent);
+        
+        if(rank[pa-'a']> rank[pa-'a']){
+            parent[pb-'a']=pa;
+        }else if(rank[pa-'a']< rank[pa-'a']){
+             parent[pa-'a']=pb;
+        }else{
+            parent[pa-'a']=pb;
+            rank[pb-'a']++;
         }
     }
 }
