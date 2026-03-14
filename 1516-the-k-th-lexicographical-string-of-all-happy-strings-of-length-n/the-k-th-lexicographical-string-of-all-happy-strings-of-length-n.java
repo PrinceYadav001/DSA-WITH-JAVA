96029e7473c9389ad4f1b279ab4e0fa1bc1503e9
Prince Yadav <150104887+PrinceYadav001@users.noreply.github.com>
2026-03-14 23:55:15 +0530

    Time: 23 ms (18.70%) | Memory: 47 MB (24.43%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<String> list = new ArrayList<>();
+    public String getHappyString(int n, int k) {
+       generate("" , n);
+       if(k>list.size()) return "";
+       return list.get(k-1);
+    }
+    public void generate(String s , int n){
+        if(s.length()==n){
+            list.add(s);
+            return;
+        }
+        for(char c : new char[]{'a' , 'b' , 'c'}){
+            if(s.length()==0 || s.charAt(s.length()-1)!=c){
+                generate(s+c, n);
+            }
+        }
+    }
+}
