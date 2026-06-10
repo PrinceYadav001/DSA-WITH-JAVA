100a12caf8d12df7fcd80da09d37362d353e28fc
Prince Yadav <150104887+PrinceYadav001@users.noreply.github.com>
2026-06-10 23:38:54 +0530

    Time: 45 ms (23.53%) | Memory: 46.9 MB (82.57%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,13 +1,45 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer ,Integer> map = new HashMap<>();
-        for(int i =0 ; i<nums.length; i++){
-            int n = target-nums[i];
-            if(map.containsKey(n)){
-                return new int[] {map.get(n) , i};
+        int sum =0;
+        for(int i=0 ; i<nums.length; i++){
+            for(int j = i+1 ;j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i , j};
+                }
             }
-            map.put(nums[i], i);
         }
-        return new int[] { };
+        return new int[]{-1 , -1};
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int arr[] = new int[2];
+//         for(int i =0 ; i <nums.length ; i++){
+//             for(int j =i+1 ; j<nums.length ; j++){
+//                 if(nums[i]+nums[j]== target){
+//                     arr[0] =i;
+//                     arr[1] =j;
+//                     return arr;
+//                 }
+//             }
+//         }
+//         return arr;
+//     }
+// }
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer , Integer> map = new HashMap<>();
+//         for(int i=0 ; i<nums.length ; i++){
+//             int sum = target-nums[i];
+//             if(map.containsKey(sum)){
+//                 return new int[]{map.get(sum) , i};
+//             }else{
+//                 map.put(nums[i] , i);
+//             }
+//         }
+//         return  new int[]{-1 , -1};
+//     }
+// }
